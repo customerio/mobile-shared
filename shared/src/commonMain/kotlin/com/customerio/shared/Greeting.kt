02c9e4477af7d38b8f18600e9c6ff53ef8b1e557
Shahroz Khan <shrozali@gmail.com>
2022-10-26 16:40:17 +0500

    Added empty repo

---
+++ shared/src/commonMain/kotlin/com/customerio/shared/Greeting.kt
@@ -0,0 +1,9 @@
+package com.customerio.shared
+
+class Greeting {
+    private val platform: Platform = getPlatform()
+
+    fun greeting(): String {
+        return "Hello, ${platform.name}!"
+    }
+}
